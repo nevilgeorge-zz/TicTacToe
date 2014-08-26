0c4952d0eec384afa947caf684242563a1801b28
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-08-25 23:35:23 -0700

    two more functions added to class

--- TicTacToe.py
+++ TicTacToe.py
@@ -7,5 +7,30 @@ class TicTacToe(object):
 
 	outcomes = ('X-win', 'Draw', 'O-win')
 
+	# Constructor of the class
 	def __init__(self, squares = []):
-		
+		if (len(squares) == 0):
+			for i in range(9):
+				self.squares[i] = None
+		else:
+			self.squares = squares
+
+	# Shows the board
+	def show(self):
+		length = len(self.squares)
+		for i in range(length):
+			if (i % 3 == 0):
+				print '\n'
+				print self.squares[i]
+			else:
+				print self.squares[i]
+
+	# All empty spaces available on the board
+	# Returns an array of the indices where there is a free space
+	def available_spaces(self):
+		emptySpaces = []
+		for i in range(len(self.squares)):
+			if (self.squares[i] == None):
+				emptySpaces.append(i);
+
+	
